1509988d4378ef84ae45400b1cdbd4df7531de9e
itsHazeyo <72316312+itsHazeyo@users.noreply.github.com>
2023-05-16 14:44:41 -0400

    Current FiveM Version 1.9.9.1
    All of the files Yeah baby

---
+++ FiveM version/unlimited/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+name 'GTA:Unlimted Handling mod'
+author 'itsHazeyo aka Convertible Summers'
+description 'the Midnight Club of Los Santos'
+version 'v1.9.9.1 Drug Wars FiveM Standard'
+url 'https://www.gta5-mods.com/vehicles/gta5-unlimited'
+
+files {
+    'handling.meta',
+    'vehicles.meta',
+
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+
+ 
+this_is_a_map 'yes'
